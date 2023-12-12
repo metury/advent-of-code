c31964dcfd91b3a78b01591c16333b6219a76ad7
Tomas Turek <tom.turek@atlas.cz>
2023-12-12 22:40:04 +0100

    finished day 12

--- 2023/12/main.py
+++ 2023/12/main.py
@@ -1,102 +1,63 @@
 #!/usr/bin/env python3
 
-first_sum = 0
-second_sum = 0
+def count_placements(groups, clusters, cache = dict()):
+	key = "|".join(map(str, groups))
+	key += "#" + ":".join(clusters)
+	if key in cache:
+		return cache[key]
 
-def count(array):
-	counter = []
-	current = 0
-	for c in array:
-		if c == "." and current > 0:
-			counter.append(current)
-			current = 0
-		if c == "#":
-			current += 1
-	if current > 0:
-		counter.append(current)
-	#print(f"Ar: {array}, counter: {counter}")
-	return counter
-
-def compare(counter, image):
-	if len(counter) != len(image):
-		return False
-	for i in range(len(counter)):
-		if counter[i] != image[i]:
-			return False
-	return True
-
-def try_fast(array, image, index, imdex, counter):
-	#print(f"Array: {array}, image: {image}, index: {index}, imdex: {imdex}, counter: {counter}")
-	if index >= len(array) and imdex >= len(image) and counter == 0:
-		#print("True")
-		return 1
-	if index >= len(array) and imdex == len(image) - 1 and image[imdex] == counter:
-		#print("True")
-		return 1
-	if index >= len(array):
-		#print("False")
-		return 0
-	if array[index] == ".":
-		if counter > 0:
-			if imdex >= len(image) or counter != image[imdex]:
-				#print("False")
+	if not groups:
+		for cluster in clusters:
+			if '#' in cluster:
 				return 0
-			if counter == image[imdex]:
-				return try_fast(array, image, index + 1, imdex + 1, 0)
-		return try_fast(array, image, index + 1, imdex, 0)
-	if array[index] == "#":
-		if imdex >= len(image) or counter + 1 > image[imdex]:
-			#print("False")
-			return 0
-		return try_fast(array, image, index + 1, imdex, counter + 1)
-	ar1 = array.copy()
-	ar1[index] = "."
-	ar2 = array.copy()
-	ar2[index] = "#"
-	if counter == 0:
-		return try_fast(ar1, image, index + 1, imdex, counter) + try_fast(ar2, image, index + 1, imdex, counter + 1)
-	if imdex < len(image) and counter == image[imdex]:
-		return try_fast(ar1, image, index + 1, imdex + 1, 0)
-	if imdex < len(image) and counter < image[imdex]:
-		return try_fast(ar2, image, index + 1, imdex, counter + 1)
-	#print("False")
-	return 0
-
-def try_all(array, image, index):
-	while index < len(array) and array[index] != "?":
-		index += 1
-	if index >= len(array) or (index == len(array) - 1 and array[index] != "?"):
-		if compare(count(array), image):
-			return 1
+		return 1
+	if not clusters:
 		return 0
-	ar1 = array.copy()
-	ar2 = array.copy()
-	ar1[index] = "."
-	ar2[index] = "#"
-	return try_all(ar1, image, index + 1) + try_all(ar2, image, index + 1)
+	ret = 0
+	cluster = clusters[0]
+	group = groups[0]
+	len_cluster = len(cluster)
+	if group > len_cluster and '#' in cluster:
+		return 0
+	for i in range(len_cluster - group + 1):
+		left = cluster[:i]
+		if '#' in left:
+			continue
+		right = cluster[i + group:]
+		if right.startswith('#'):
+			continue
+		new_clusters = clusters[1:]
+		if len(right) > 1:
+			new_clusters.insert(0, right[1:])
+		ret += count_placements(groups[1:], new_clusters, cache)
 
-def multiply(array, image):
-	origin = array.copy()
-	origin_image = image.copy()
-	for i in range(4):
-		array.append("?")
-		for c in origin:
-			array.append(c)
-		for i in origin_image:
-			image.append(i)
+	if '#' not in cluster:
+		ret += count_placements(groups, clusters[1:], cache)
 
-print("[",end="")
+	cache[key] = ret
 
-with open('INPUT') as f:
-	for line in f:
-		parts = line.strip().split(" ")
-		array = [x for x in parts[0]]
-		image = [int(x) for x in parts[1].split(",")]
-		first_sum += try_fast(array, image, 0, 0, 0)
-		multiply(array, image)
-		second_sum += try_fast(array, image, 0, 0, 0)
-		print("-",end="")
-print("]")
+	return ret
 
-print(f"First part: {first_sum}")
-print(f"Second part: {second_sum}")
+
+def compute(expand):
+	total = 0
+	with open('INPUT', 'r') as file:
+		for line in file:
+			springs, groups_str = line.strip().split()
+			groups = [int(x) for x in groups_str.split(",")]
+			if expand:
+				springs = "?".join(5 * [springs])
+				groups = 5 * groups
+			clusters = [s for s in springs.split('.') if s]
+			delta = count_placements(groups, clusters)
+			total += delta
+	return total
+
+def part1():
+	return compute(False)
+
+def part2():
+	return compute(True)
+
+print(f"First part: {part1()}")
+print(f"Second part: {part2()}")
