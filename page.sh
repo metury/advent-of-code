81401b8384917ef518a376b35d4c0e18a1dadab8
Tomas Turek <tom.turek@atlas.cz>
2023-12-14 20:31:44 +0100

    updated page creation

--- page.sh
+++ page.sh
@@ -2,14 +2,22 @@
 
 set -ueo pipefail
 
+## ==================== ##
+##  Constants           ##
+## ==================== ##
+
 aoc="advent of code"
 link="/aoc/"
 aoc_dir="aoc"
 aoc_file="adventofcode.md"
 
-mkdir -p "$aoc_dir"
+## ==================== ##
+##  Functions           ##
+## ==================== ##
 
-echo "---
+# Print the title page.
+print_aoc(){
+	echo "---
 layout: page
 title: $aoc
 permalink: $link
@@ -18,9 +26,58 @@ has_children: true
 
 They are my solutions to [advent of code](https://adventofcode.com/) tasks. There are separated to each year and day. All of this can be found on [GitHub](https://github.com/metury/advent-of-code), also with the script that generates these pages.
 
-Plus you may also play a small [Bingo](https://aoc-bingo.fly.dev/) that someone made. :D
+Plus you may also play a small [Bingo](https://aoc-bingo.fly.dev/) that someone made. Also you may consider joining [Reddit](https://www.reddit.com/r/adventofcode/) where you may find useful tips, or help someone.
 
+### Years
 " > "$aoc_file"
+}
+
+# Print year page by: number.
+print_year(){
+	echo "---
+layout: page
+title: Year $1
+parent: $aoc
+permalink: $link$1/
+has_children: true
+---
+
+This contains tasks from the year $dir.
+
+### Days
+" > ../"$aoc_dir"/"$1".md
+}
+
+# Print day by its number and number of year.
+print_day(){
+	echo "---
+layout: page
+title: Day $1
+parent: Year $2
+grand_parent: $aoc
+permalink: $link$2/$1/
+---
+
+This is a solution of the day $1.
+
+" > "$3"
+}
+
+# File, and where to print it.
+print_file(){
+	echo "# $1" >> "$2"
+	echo "" >> "$2"
+	echo "\`\`\`$(echo "$1" | cut -d. -f2)" >> "$2"
+	expand -i -t2 "$1" >> "$2"
+	echo "\`\`\`" >> "$2"
+}
+
+## ==================== ##
+##  Main loop           ##
+## ==================== ##
+
+mkdir -p "$aoc_dir"
+print_aoc
 
 for dir in *; do
 	if [[ "$dir" == "$aoc_dir" ]]; then
@@ -28,42 +85,27 @@ for dir in *; do
 	fi
 	if [ -d "$dir" ]; then
 		echo "- [Year $dir]($link$dir)" >> "$aoc_file"
-		cd "$dir" # move to the top level directory.
-		echo "---
-layout: page
-title: Year $dir
-parent: $aoc
-permalink: $link$dir/
-has_children: true
----
-
-This contains tasks from the year $dir.
-
-" > ../"$aoc_dir"/"$dir".md
+		cd "$dir"
+		print_year "$dir"
 		for subdir in *; do
 			if [ -d "$subdir" ]; then
 				echo "- [Day $subdir]($link$dir/$subdir/)" >> ../"$aoc_dir"/"$dir".md
 				cd "$subdir" # move to the inner directory
 				out="../../$aoc_dir/$dir-$subdir.md"
-				echo "---
-layout: page
-title: Day $subdir
-parent: Year $dir
-grand_parent: $aoc
-permalink: $link$dir/$subdir/
----
-
-This is a solution of the day $subdir.
-
-" > "$out"
+				print_day "$subdir" "$dir" "$out"
 				for file in *; do
-					if [ "$file" != "INPUT" ] && [ "$file" != "main" ]; then
-						echo "# $file" >> "$out"
-						echo "" >> "$out"
-						echo "\`\`\`$(echo "$file" | cut -d. -f2)" >> "$out"
-						expand -i -t2 "$file" >> "$out"
-						echo "\`\`\`" >> "$out"
-						echo "" >> "$out"
+					if [ "$file" != "INPUT" ] && [ "$file" != "main" ] && [ "$file" != "Cargo.lock" ] && [ "$file" != "Cargo.toml" ]; then
+						if [ -d "$file" ]; then
+							if [ "$file" == "src" ]; then
+								cd "$file"
+								for src in *; do
+									print_file "$src" "../$out"
+								done
+								cd ..
+							fi
+						else
+							print_file "$file" "$out"
+						fi
 					fi
 				done
 				cd ..
