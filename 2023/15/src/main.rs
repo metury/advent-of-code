e51bb20e0871d2de294c9aeba74ca390f10a9686
Tomas Turek <tom.turek@atlas.cz>
2023-12-15 12:14:43 +0100

    update to day 15

--- 2023/15/src/main.rs
+++ 2023/15/src/main.rs
@@ -26,6 +26,14 @@ fn ascii_hash(string: &str) -> i32{
 	return hash;
 }
 
+fn insert_to_map(hash_map: &mut [LinkedList<i32>; 256], lens: i32, key: &str){
+	let hash = ascii_hash(key);
+	let list = &hash_map[hash as usize];
+	if list.contains(&hash){
+		// This is actually nonsense.
+	}
+}
+
 
 fn part1(){
 	let strings = read_file("INPUT");
@@ -37,14 +45,28 @@ fn part1(){
 }
 
 fn part2(){
-	let mut hash_map: [LinkedList<i32>; 256];
+	let mut hash_map: [LinkedList<i32>; 256] = [LinkedList::new(); 256];
 	let strings = read_file("INPUT");
 	let mut total = 0;
 	for string in strings{
 		if string.chars().nth(string.len() - 1) == Some('-'){
-			println!("S");
+			let key = &string[0..string.len() - 2];
+			// Remove.
+		}
+		else if string.chars().nth(string.len() - 2) == Some('='){
+			let lens = string.chars().nth(string.len() - 1).unwrap();
+			let key = &string[0..string.len() - 3];
+			// Insert
 		}
 	}
+	for i in 0..hash_map.len(){
+		let mut j = 0;
+		for element in hash_map[i]{
+			total += (i + 1) * (j + 1) * (element as usize);
+			j += 1;
+		}
+	}
+	println!("Part 2: {}", total);
 }
 
 fn main() {
