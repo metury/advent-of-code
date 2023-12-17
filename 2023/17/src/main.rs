806817ab827f6b138fcea1c3f7e7c3121ba37cd0
Tomas Turek <tom.turek@atlas.cz>
2023-12-17 14:36:01 +0100

    update

--- 2023/17/src/main.rs
+++ 2023/17/src/main.rs
@@ -1,8 +1,6 @@
 use std::fs;
-use std::collections::BinaryHeap;
-use std::cmp::Ordering;
-
-const LIMIT: i8 = 3;
+use petgraph::graph::{Graph, NodeIndex};
+use petgraph::algo::dijkstra;
 
 fn read_file(filepath: &str) -> (Vec<Vec<i8>>, Vec<Vec<i64>>){
 	let contents = fs::read_to_string(filepath);
@@ -23,115 +21,59 @@ fn read_file(filepath: &str) -> (Vec<Vec<i8>>, Vec<Vec<i64>>){
 		}
 	}
 	return (grid, paths);
+
 }
 
 struct Node{
-	len: i64,
 	position: (usize, usize),
 	movement: (i64, i64),
 	limit: i8,
 }
 
-impl Ord for Node{
-	fn cmp(&self, other: &Self) -> Ordering {
-		(self.len).cmp(&other.len)
-	}
-}
-
-impl PartialOrd for Node {
-	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-		Some(self.cmp(other))
-	}
-}
-
-impl PartialEq for Node {
-	fn eq(&self, other: &Self) -> bool {
-		self.len == other.len
-	}
-}
-
-impl Eq for Node { }
-
-fn create_new_node(grid: &Vec<Vec<i8>>, paths: &mut Vec<Vec<i64>>, node: &Node, heap: &mut BinaryHeap<Node>){
-	let len = node.len;
-	let pos = node.position;
-	let mov = node.movement;
-	if pos.0 >= grid.len() || pos.1 >= grid[pos.0].len(){
-		return;
-	}
-	if paths[pos.0][pos.1] < len{
-		return;
-	}
-	let mut new_pos = pos;
-	let mut new_mov = mov;
-	let mut new_len = len;
-
-	if node.limit < LIMIT {
-		new_pos = ((pos.0 as i64 + new_mov.0) as usize, (pos.1 as i64 + new_mov.1) as usize);
-		if new_pos.0 < grid.len() && new_pos.1 < grid[pos.0].len() {
-			new_len = len + grid[new_pos.0][new_pos.1] as i64;
-			heap.push(Node{len: new_len, position: new_pos, movement: new_mov, limit: node.limit + 1});
+fn create_graph(grid: &Vec<Vec<i8>>) -> (Graph<Node, i64>, NodeIndex) {
+	let mut graph = Graph::<Node,i64>::new();
+	let moves = [(0,1), (0,-1), (1,0), (-1,0)];
+	let mut nodes: Vec<(Node, NodeIndex)> = vec!();
+	for i in 0..grid.len() {
+		for j in 0..grid[i].len() {
+			for m in moves {
+				for k in 0..3 {
+					let a = graph.add_node(Node{position: (i,j), movement: m, limit: k});
+					nodes.push((Node{position: (i,j), movement: m, limit: k}, a));
+				}
+			}
 		}
 	}
-	new_mov = (mov.1, mov.0);
-	new_pos = ((pos.0 as i64 + new_mov.0) as usize, (pos.1 as i64 + new_mov.1) as usize);
-	if new_pos.0 < grid.len() && new_pos.1 < grid[pos.0].len() {
-		new_len = len + grid[new_pos.0][new_pos.1] as i64;
-		heap.push(Node{len: new_len, position: new_pos, movement: new_mov, limit: 1});
+	for i in 0..nodes.len() - 1 {
+		for j in 0..nodes.len() - 1 {
+			let pos_i = nodes[i].0.position;
+			let pos_j = nodes[j].0.position;
+			let mov_j = nodes[j].0.movement;
+			let limit_i = nodes[j].0.limit;
+			let limit_j = nodes[j].0.limit;
+			let same_row: bool = pos_i.0 as i64 + mov_j.0 == pos_j.0 as i64
+			                  && pos_i.1 as i64 + mov_j.1 == pos_j.1 as i64
+			                  && limit_i < 2 && limit_i + 1 == limit_j;
+			let left: bool = pos_i.0 as i64 + mov_j.1 == pos_j.0 as i64
+			              && pos_j.0 as i64 + mov_j.0 == pos_j.0 as i64
+			              && limit_j == 0;
+			let right: bool = pos_i.0 as i64 - mov_j.1 == pos_j.0 as i64
+			               && pos_j.0 as i64 - mov_j.0 == pos_j.0 as i64
+			               && limit_j == 0;
+			if same_row || left || right {
+				graph.add_edge(nodes[i].1, nodes[j].1, grid[pos_j.0][pos_j.1] as i64);
+			}
+		}
 	}
-	new_mov = (-mov.1, -mov.0);
-	new_pos = ((pos.0 as i64 + new_mov.0) as usize, (pos.1 as i64 + new_mov.1) as usize);
-	if new_pos.0 < grid.len() && new_pos.1 < grid[pos.0].len() {
-		new_len = len + grid[new_pos.0][new_pos.1] as i64;
-		heap.push(Node{len: new_len, position: new_pos, movement: new_mov, limit: 1});
-	}
-}
-
-fn dijkstra(grid: &Vec<Vec<i8>>, paths: &mut Vec<Vec<i64>>){
-	let mut heap: BinaryHeap<Node> = BinaryHeap::new();
-	paths[0][0] = grid[0][0] as i64;
-	let path = paths[0][0];
-	let mut node: Node = Node{ len: path + grid[0][1] as i64, position: (0,1), movement: (0,1), limit: 1};
-	heap.push(node);
-	node = Node{ len: path + grid[1][0] as i64, position: (1,0), movement: (1,0), limit: 1};
-	heap.push(node);
-	while heap.peek() != None{
-		let Some(node) = heap.pop() else { return; };
-		create_new_node(grid, paths, &node, &mut heap);
-	}
-}
-
-fn step(grid: &Vec<Vec<i8>>, paths: &mut Vec<Vec<i64>>, position: (usize, usize), movement: (i8, i8), limit: i8, path: i64){
-	let out_of_bounds: bool = position.0 >= grid.len() || position.1 >= grid[position.0].len();
-	if out_of_bounds || paths[position.0][position.1] < path + grid[position.0][position.1] as i64{
-		return;
-	}
-	let extended_path = path + grid[position.0][position.1] as i64;
-	paths[position.0][position.1] = extended_path;
-	let new_pos = ((position.0 as i64 + movement.0 as i64) as usize, (position.1 as i64 + movement.1 as i64) as usize);
-	if limit < 3{
-		step(grid, paths, new_pos, movement, limit + 1, extended_path);
-	}
-	let mut new_movement = (movement.1, movement.0);
-	step(grid, paths, new_pos, new_movement, 0, extended_path);
-	new_movement = (-movement.1, -movement.0);
-	step(grid, paths, new_pos, new_movement, 0, extended_path);
-}
-
-fn first_step(grid: &Vec<Vec<i8>>, paths: &mut Vec<Vec<i64>>){
-	let position: (usize, usize) = (0,0);
-	let mut movement: (i8, i8) = (0,1);
-	step(grid, paths, position, movement, 0, 0);
-	movement = (1,0);
-	step(grid, paths, position, movement, 0, 0);
+	return (graph, nodes[0].1);
 }
 
 fn part1(){
 	let (grid, mut paths) = read_file("INPUT");
-	//first_step(&grid, &mut paths);
-	dijkstra(&grid, &mut paths);
-	let result = paths[paths.len() - 1][paths[paths.len() - 1].len() - 1];
-	println!("Part 1: {}", result);
+	let (graph, start_node) = create_graph(&grid);
+	let distances = dijkstra(&graph, start_node, None, |_| 1);
+	println!("{:?}", distances);
+	println!("Part 1: {}", 0);
 }
 
 fn part2(){
