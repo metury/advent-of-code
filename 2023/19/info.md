0fc5c9cd1b12014bf5595a50cec68a980e248f80
Tomas Turek <tom.turek@atlas.cz>
2023-12-19 21:57:50 +0100

    done day 19

--- 2023/19/info.md
+++ 2023/19/info.md
@@ -1,4 +1,9 @@
+The main key was to parse properly the given input. That is why the code today is pretty long.
+
 #### Part 1
+
+After that we just use hash table to memorize given states. Than we process each request from left to right until it is satisfied.
 
 #### Part 2
 
+In the second part we had to use the same aproach only to compute the intervals not just single numbers. But it is very similiar, since we just split parts on each comparison and recursively called to the next step.
--- 2023/19/src/main.rs
+++ 2023/19/src/main.rs
@@ -1,23 +1,23 @@
 use std::fs;
 use std::collections::HashMap;
+use std::cmp::{min, max};
 
 const X: usize = 0;
 const M: usize = 1;
 const A: usize = 2;
 const S: usize = 3;
+const INV: usize = 5;
 
 type Part = [i64; 4];
 
 type BoundPart = [(i64,i64); 4];
 
-#[derive(Debug)]
 enum Compare {
 	LessThan,
 	MoreThan,
 	True,
 }
 
-#[derive(Debug)]
 struct Rule {
 	considering: usize,
 	compare: Compare,
@@ -45,16 +45,28 @@ fn parse_rule(line: &str) -> Rule {
 	if parts.len() == 1 as usize {
 		let new_state = parts[0];
 		if new_state == "A" {
-			return Rule { considering: 5, compare: Compare::True, number: 0,
-	              next_state: "".to_string(), accepting: true, rejecting: false};
+			return Rule {considering: INV,
+				compare: Compare::True,
+				number: 0,
+				next_state: "".to_string(),
+				accepting: true,
+				rejecting: false};
 		}
 		else if new_state == "R" {
-			return Rule { considering: 5, compare: Compare::True, number: 0,
-	              next_state: "".to_string(), accepting: false, rejecting: true};
+			return Rule {considering: INV,
+				compare: Compare::True,
+				number: 0,
+				next_state: "".to_string(),
+				accepting: false,
+				rejecting: true};
 		}
 		else {
-			return Rule { considering: 5, compare: Compare::True, number: 0,
-	              next_state: new_state.to_string(), accepting: false, rejecting: false};
+			return Rule {considering: INV,
+				compare: Compare::True,
+				number: 0,
+				next_state: new_state.to_string(),
+				accepting: false,
+				rejecting: false};
 		}
 	}
 	let binding = parts[0].to_string();
@@ -68,16 +80,28 @@ fn parse_rule(line: &str) -> Rule {
 	let number = i64::from_str_radix(&binding[2 ..], 10).unwrap();
 	let new_state = parts[1].to_string();
 	if new_state == "A" {
-		return Rule { considering: considering, compare: compare, number: number,
-	              next_state: "".to_string(), accepting: true, rejecting: false};
+		return Rule {considering: considering,
+			compare: compare,
+			number: number,
+			next_state: "".to_string(),
+			accepting: true,
+			rejecting: false};
 	}
 	else if new_state == "R" {
-		return Rule { considering: considering, compare: compare, number: number,
-	              next_state: "".to_string(), accepting: false, rejecting: true};
+		return Rule {considering: considering,
+			compare: compare,
+			number: number,
+			next_state: "".to_string(),
+			accepting: false,
+			rejecting: true};
 	}
 	else {
-		return Rule { considering: considering, compare: compare, number: number,
-	              next_state: new_state, accepting: false, rejecting: false};
+		return Rule {considering: considering,
+			compare: compare,
+			number: number,
+			next_state: new_state,
+			accepting: false,
+			rejecting: false};
 	}
 }
 
@@ -177,50 +201,69 @@ fn process_part(hash_map: &HashMap<String, State>, part: Part) -> i64 {
 	}
 }
 
-fn process_bounds(hash_map: &HashMap<String, State>, bounds: BoundPart, string: &str) -> i64 {
-	loop {
-		let state = &hash_map[string];
-		for rule in state {
-			match rule.compare {
-				Compare::LessThan => {
-					let part1 = (bounds[rule.considering].0, rule.number);
-					let part2 = (rule.number, bounds[rule.considering].1);
-					if part[rule.considering] < rule.number {
-						if rule.accepting {
-							return ret;
-						}
-						else if rule.rejecting {
-							return 0;
-						}
-						string = &rule.next_state;
-						break;
-					}
-				}
-				Compare::MoreThan => {
-					if part[rule.considering] > rule.number {
-						if rule.accepting {
-							return ret;
-						}
-						else if rule.rejecting {
-							return 0;
-						}
-						string = &rule.next_state;
-						break;
-					}
-				}
-				Compare::True => {
+fn check_bound(bound: (i64, i64)) -> bool {
+	bound.1 > bound.0
+}
+
+fn count_possibilites(bounds: &BoundPart) -> i64 {
+	let res = (bounds[X].1 - bounds[X].0) * (bounds[M].1 - bounds[M].0) * (bounds[A].1 - bounds[A].0) * (bounds[S].1 - bounds[S].0);
+	res
+}
+
+fn process_bounds(hash_map: &HashMap<String, State>, bounds: &mut BoundPart, string: &str) -> i64 {
+	let mut total: i64 = 0;
+	let state = &hash_map[string];
+	for rule in state {
+		match rule.compare {
+			Compare::LessThan => {
+				let part1 = (bounds[rule.considering].0, rule.number);
+				let mut new_bounds = [bounds[X], bounds[M], bounds[A], bounds[S]];
+				bounds[rule.considering] = (max(rule.number, bounds[rule.considering].0), bounds[rule.considering].1);
+				new_bounds[rule.considering] = part1;
+				if check_bound(part1){
 					if rule.accepting {
-						return ret;
+						total += count_possibilites(&new_bounds);
 					}
 					else if rule.rejecting {
-						return 0;
+						total += 0;
 					}
-					string = &rule.next_state;
-					break;
+					else {
+						total += process_bounds(hash_map, &mut new_bounds, &rule.next_state);
+					}
+				}
+			}
+			Compare::MoreThan => {
+				let part1 = (rule.number + 1, bounds[rule.considering].1);
+				let mut new_bounds = [bounds[X], bounds[M], bounds[A], bounds[S]];
+				bounds[rule.considering] = (bounds[rule.considering].0, min(rule.number + 1, bounds[rule.considering].1));
+				new_bounds[rule.considering] = part1;
+				if check_bound(part1){
+					if rule.accepting {
+						total += count_possibilites(&new_bounds);
+					}
+					else if rule.rejecting {
+						total += 0;
+					}
+					else {
+						total += process_bounds(hash_map, &mut new_bounds, &rule.next_state);
+					}
+				}
+			}
+			Compare::True => {
+				let mut new_bounds = [bounds[X], bounds[M], bounds[A], bounds[S]];
+				if rule.accepting {
+					total += count_possibilites(&bounds);
+				}
+				else if rule.rejecting {
+					total += 0;
+				}
+				else {
+					total += process_bounds(hash_map, &mut new_bounds, &rule.next_state);
 				}
 			}
 		}
 	}
+	total
 }
 
 fn part1() {
@@ -231,8 +274,8 @@ fn part1() {
 
 fn part2() {
 	let (hash_map, _) = read_file("INPUT");
-	let part: BoundPart = [(1, 4000); 4];
-	println!("Part 2: {}", process_bounds(hash_map, part, "in"));
+	let mut part: BoundPart = [(1, 4001); 4];
+	println!("Part 2: {}", process_bounds(&hash_map, &mut part, "in"));
 }
 
 fn main() {
