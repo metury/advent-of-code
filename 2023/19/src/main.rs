13b5b7ee1d39c9d79f51805cb32bfa2a03122fcb
Tomáš Turek <turekto1@u-pl2.ms.mff.cuni.cz>
2023-12-19 11:37:30 +0100

    update parser

--- 2023/19/src/main.rs
+++ 2023/19/src/main.rs
@@ -8,31 +8,105 @@ struct Part {
 	s: i64,
 }
 
+#[derive(Debug)]
 enum Compare {
-	LESS_THAN,
-	MORE_THAN,
+	LessThan,
+	MoreThan,
+	True,
 }
 
+#[derive(Debug)]
 struct Rule {
 	considering: char,
 	compare: Compare,
+	number: i64,
 	next_state: String,
 	accepting: bool,
 	rejecting: bool,
 }
 
-fn read_file(filepath: &str) -> (HashMap<String, Rule>, Vec<Part>) {
+struct State {
+	rules: Vec<Rule>,
+}
+
+fn parse_rule(line: &str) -> Rule {
+	let splitted = line.split(':');
+	let parts: Vec<&str> = splitted.collect();
+	if parts.len() == 1 as usize {
+		let new_state = parts[0];
+		if new_state == "A" {
+			return Rule { considering: '@', compare: Compare::True, number: 0,
+	              next_state: "".to_string(), accepting: true, rejecting: false};
+		}
+		else if new_state == "R" {
+			return Rule { considering: '@', compare: Compare::True, number: 0,
+	              next_state: "".to_string(), accepting: false, rejecting: true};
+		}
+		else {
+			return Rule { considering: '@', compare: Compare::True, number: 0,
+	              next_state: new_state.to_string(), accepting: false, rejecting: false};
+		}
+	}
+	let binding = parts[0].to_string();
+	let considering = binding.chars().nth(0).unwrap();
+	let mut compare: Compare;
+	match binding.chars().nth(1).unwrap() {
+		'<' => compare = Compare::LessThan,
+		'>' => compare = Compare::MoreThan,
+		_   => compare = Compare::True,
+	}
+	let number = i64::from_str_radix(&binding[2 ..], 10).unwrap();
+	let new_state = parts[1].to_string();
+	if new_state == "A" {
+		return Rule { considering: considering, compare: compare, number: number,
+	              next_state: "".to_string(), accepting: true, rejecting: false};
+	}
+	else if new_state == "R" {
+		return Rule { considering: considering, compare: compare, number: number,
+	              next_state: "".to_string(), accepting: false, rejecting: true};
+	}
+	else {
+		return Rule { considering: considering, compare: compare, number: number,
+	              next_state: new_state, accepting: false, rejecting: false};
+	}
+}
+
+fn parse_state(line: &str) -> (String, State) {
+	let mut parts = line.split('{');
+	let string = parts.next().unwrap().to_string();
+	let mut rules: Vec<Rule> = vec!();
+	let binding = parts.next().unwrap().to_string();
+	let rule = &binding[0 .. binding.len() - 1];
+	for r in rule.split(',') {
+		rules.push(parse_rule(r));
+	}
+	println!("String {} and rules {:?}", string, rules);
+	(string, State{rules: rules})
+}
+
+fn read_file(filepath: &str) -> (HashMap<String, State>, Vec<Part>) {
 	let contents = fs::read_to_string(filepath);
 	let binding = contents.expect("REASON");
 	let lines = binding.split('\n');
+	let mut parts: Vec<Part> = vec!();
+	let mut hash_map: HashMap<String, State> = HashMap::new();
+	let mut first_part: bool = true;
 	for line in lines{
-		for c in line.chars(){
+		if line == "" {
+			first_part = false;
+		}
+		else if first_part {
+			parse_state(&line);
+		}
+		else {
+			// Parse Part.
 		}
 	}
-	return;
+	(hash_map, parts)
 }
 
 fn part1() {
+	read_file("INPUT");
 	println!("Part 1: {}", 0);
 }
 
