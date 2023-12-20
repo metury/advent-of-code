4158e20a8afb1c6c7fe4d5fff836e750a2d6a0c6
Tomas Turek <tom.turek@atlas.cz>
2023-12-20 10:59:09 +0100

    update

--- 2023/20/src/main.rs
+++ 2023/20/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, VecDeque, HashSet};
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 enum NodeType {
@@ -8,13 +8,19 @@ enum NodeType {
 	Broadcast,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Pulse {
+	Low,
+	High,
+}
+
 #[derive(Debug, Clone)]
 struct Node {
 	name: String,
 	node_type: NodeType,
 	on: bool,
 	next_nodes: Vec<String>,
-	predecessors: HashMap<String, bool>,
+	predecessors: HashMap<String, Pulse>,
 }
 
 fn parse_node(line: &str) -> Node {
@@ -53,16 +59,57 @@ fn read_file(filepath: &str) -> HashMap<String, Node> {
 
 fn find_predecessors(hash_map: &mut HashMap<String, Node>) {
 	let mut name = "broadcaster".to_string();
+	let mut set: HashSet<String> = HashSet::new();
 	let mut node: &Node;
 	let mut queue: VecDeque<String> = VecDeque::new();
 	queue.push_back(name.clone());
 	while !queue.is_empty() {
 		name = queue.pop_front().unwrap().to_string();
-		node = &hash_map[&name];
-		for n in &node.next_nodes {
-			//let new_node = &hash_map[n];
-			//new_node.predecessors.insert(name.clone(), false);
-			queue.push_back(n.clone()); // Cycles
+		if set.contains(&name) {
+			continue;
+		}
+		set.insert(name.clone());
+		let next_nodes = &hash_map[&name].next_nodes.clone();
+		for n in next_nodes {
+			let mut new_node = hash_map.get_mut(n).unwrap();
+			new_node.predecessors.insert(name.clone(), Pulse::Low);
+			queue.push_back(n.clone());
+		}
+	}
+}
+
+fn all_highs(hash_map: &HashMap<String, Pulse>) -> bool {
+	hash_map.into_iter().map(|x| x.1 == &Pulse::High).fold(true, |acc, x| acc && x)
+}
+
+fn bfs(hash_map: &HashMap<String, Node>) {
+	let mut name = "broadcaster".to_string();
+	let mut pulse: Pulse = Pulse::Low;
+	let mut node: &Node;
+	let mut queue: VecDeque<(String, Pulse)> = VecDeque::new();
+	queue.push_back((name.clone(), pulse));
+	while !queue.is_empty() {
+		(name, pulse) = queue.pop_front().unwrap().to_string();
+		let next_nodes = &hash_map[&name].next_nodes.clone();
+		match node.node_type {
+			NodeType::FlipFlop => {
+				if pulse == Pulse::Low {
+					node.on = !node.on;
+					if node.on {
+						pulse = Pulse::High;
+					}
+					else {
+						pulse = Pulse::Low;
+					}
+				}
+			}
+			NodeType::Conjuction => {
+			NodeType::Broadcast => todo!(),
+		}
+		for n in next_nodes {
+			let mut new_node = hash_map.get_mut(n).unwrap();
+			// Update for conju
+			queue.push_back(n.clone());
 		}
 	}
 }
@@ -70,6 +117,9 @@ fn find_predecessors(hash_map: &mut HashMap<String, Node>) {
 fn part1() {
 	let mut hash_map = read_file("INPUT");
 	find_predecessors(&mut hash_map);
+	for node in hash_map {
+		println!("{:?}", node);
+	}
 	println!("Part 1: {}", 0);
 }
 
