e65fc315144099050298aebcbbed37bd3c1a5f29
Tomas Turek <tom.turek@atlas.cz>
2023-12-23 11:08:12 +0100

    finished day 23 with brute force

---
+++ 2023/23/src/main.rs
@@ -0,0 +1,132 @@
+use std::fs;
+use std::collections::HashSet;
+
+const EMPTY: char = '.';
+
+type Grid<T> = Vec<Vec<T>>;
+type Position = (usize, usize);
+
+fn read_file(filepath: &str) -> Grid<char> {
+	let contents = fs::read_to_string(filepath);
+	let binding = contents.expect("REASON");
+	let lines = binding.split('\n');
+	let mut map: Grid<char> = vec![];
+	for line in lines{
+		let map_line: Vec<char> = line.chars().collect();
+		if map_line.len() > 0 {
+			map.push(map_line);
+		}
+	}
+	map
+}
+
+fn find_start_end(map: &Grid<char>) -> (Position, Position) {
+	let mut start: Position = (0,0);
+	let mut end: Position = (0,0);
+	for i in 0 .. map[0].len() {
+		if map[0][i] == EMPTY {
+			start = (0, i);
+			break;
+		}
+	}
+	for i in 0 .. map[map.len() - 1].len() {
+		if map[map.len() - 1][i] == EMPTY {
+			end = (map.len() - 1, i);
+			break;
+		}
+	}
+	(start, end)
+}
+
+fn add(a: (usize, usize), b: (i8, i8)) -> (usize, usize) {
+	((a.0 as i64 + b.0 as i64) as usize, (a.1 as i64 + b.1 as i64) as usize)
+}
+
+fn step(map: &Grid<char>, position: Position) -> Vec<Position> {
+	let mut positions: Vec<Position> = vec!();
+	const LEN: usize = 4;
+	let neighbors: [(i8, i8); LEN] = [(0,-1), (0,1), (1,0), (-1,0)];
+	let slopes: [char; LEN] = ['<', '>', 'v', '^'];
+	for i in 0 .. LEN {
+		let pos: Position = add(position, neighbors[i]);
+		if pos.0 < map.len() && pos.1 < map[pos.0].len() {
+			if map[pos.0][pos.1] == EMPTY || map[pos.0][pos.1] == slopes[i] {
+				positions.push(pos);
+			}
+		}
+	}
+	positions
+}
+
+fn step_mod(map: &Grid<char>, position: Position) -> Vec<Position> {
+	let mut positions: Vec<Position> = vec!();
+	const LEN: usize = 4;
+	let neighbors: [(i8, i8); LEN] = [(0,-1), (0,1), (1,0), (-1,0)];
+	for i in 0 .. LEN {
+		let pos: Position = add(position, neighbors[i]);
+		if pos.0 < map.len() && pos.1 < map[pos.0].len() {
+			if map[pos.0][pos.1] != '#' {
+				positions.push(pos);
+			}
+		}
+	}
+	positions
+}
+
+fn longest_path(map: &Grid<char>, visited: &mut HashSet<Position>, pos: Position, max: &mut u64, len: u64, end: &Position) {
+	if pos == *end {
+		if *max < len {
+			*max = len;
+		}
+		return;
+	}
+	let neighbors = step(map, pos);
+	for n in neighbors {
+		if !visited.contains(&n) {
+			visited.insert(n);
+			longest_path(map, visited, n, max, len+1, end);
+			visited.remove(&n);
+		}
+	}
+}
+
+fn longest_path_mod(map: &Grid<char>, visited: &mut HashSet<Position>, pos: Position, max: &mut u64, len: u64, end: &Position) {
+	if pos == *end {
+		if *max < len {
+			*max = len;
+		}
+		return;
+	}
+	let neighbors = step_mod(map, pos);
+	for n in neighbors {
+		if !visited.contains(&n) {
+			visited.insert(n);
+			longest_path_mod(map, visited, n, max, len+1, end);
+			visited.remove(&n);
+		}
+	}
+}
+
+fn part1() {
+	let map = read_file("INPUT");
+	let (start, end) = find_start_end(&map);
+	let mut set: HashSet<Position> = HashSet::new();
+	let mut max: u64 = 0;
+	longest_path(&map, &mut set, start, &mut max, 0, &end); 
+	println!("Part 1: {}", max);
+}
+
+fn part2() {
+	let map = read_file("INPUT");
+	let (start, end) = find_start_end(&map);
+	let mut set: HashSet<Position> = HashSet::new();
+	let mut max: u64 = 0;
+	longest_path_mod(&map, &mut set, start, &mut max, 0, &end);
+	println!("Part 2: {}", max);
+}
+
+fn main() {
+	println!("Year 2023 day 23 - A Long Walk");
+	part1();
+	part2();
+}
