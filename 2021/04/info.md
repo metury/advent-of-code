db2d1f005d124562e5c4a36f78463b4e0dd92f2c
Tomáš Turek <tom.turek@atlas.cz>
2024-12-08 19:24:07 +0100

    redone y 2021 d 4

---
+++ 2021/04/info.md
@@ -0,0 +1,8 @@
+#### Part 1
+
+In the first part we iterate over the numbers and fill in the bingo cards and check if it is winning, if so compute the value and multiply it by the drawn number.
+
+#### Part 2
+
+In the second part use the same procedure, only cycle through the winning boards, since multiple boards can win in with the same number.
+
--- 2021/04/main.go
+++ 2021/04/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -12,48 +11,60 @@ import (
 
 type Box struct {
 	value int
-	mark bool
+	mark  bool
 }
 
 type Bingo [5][5]Box
+
+func empty_bingo() Bingo {
+	var bingo Bingo
+	return bingo
+}
 
 func read_file(file_path string) ([]int, []Bingo) {
 	content, err := os.ReadFile(file_path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	regex_numbers := regexp.MustCompile("(:?[0-9]+,)+[0-9]+")
-	numbers_string := regex_numbers.FindAllString(string(content), 1)
+	counter := 0
 	var numbers []int
-	split_numbers := strings.Split(numbers_string[0], ",")
-	for _, nbr_string := range split_numbers {
-		nr, _ := strconv.Atoi(nbr_string)
-		numbers = append(numbers, nr)
-	}
-	rows := regexp.MustCompile("([0-9]{1,2})\\s{1,2}([0-9]{1,2})\\s{1,2}([0-9]{1,2})\\s{1,2}([0-9]{1,2})\\s{1,2}([0-9]{1,2})\n")
-	found_rows := rows.FindAllStringSubmatch(string(content), -1)
 	var bingos []Bingo
-	var bingo Bingo
-	for i := 0; i < len(found_rows); i++{
-		nr1, _ := strconv.Atoi(found_rows[i][1])
-		nr2, _ := strconv.Atoi(found_rows[i][2])
-		nr3, _ := strconv.Atoi(found_rows[i][3])
-		nr4, _ := strconv.Atoi(found_rows[i][4])
-		nr5, _ := strconv.Atoi(found_rows[i][5])
-		bingo[i % 5] = [5]Box{{nr1, false}, {nr2, false}, {nr3, false}, {nr4, false}, {nr5, false}}
-		if i % 5 == 0 && i > 0 {
-			bingos = append(bingos, bingo)
+	for _, line := range strings.Split(string(content), "\n") {
+		if len(line) == 0 {
+			continue
+		}
+		space_splited := strings.Split(line, " ")
+		comma_splited := strings.Split(line, ",")
+		if len(comma_splited) > 1 {
+			for _, nbr_string := range comma_splited {
+				nr, _ := strconv.Atoi(nbr_string)
+				numbers = append(numbers, nr)
+			}
+		} else {
+			if counter%5 == 0 {
+				bingos = append(bingos, empty_bingo())
+			}
+			j := 0
+			for i := 0; i < len(space_splited); i++ {
+				if len(space_splited[i]) == 0 {
+					continue
+				}
+				nr, _ := strconv.Atoi(space_splited[i])
+				bingos[counter/5][counter%5][j] = Box{nr, false}
+				j++
+			}
+			counter++
 		}
 	}
 	return numbers, bingos
 }
 
-func is_winning(bingo Bingo) bool{
+func is_winning(bingo Bingo) bool {
 	for i, row := range bingo {
 		is_row_winning := true
 		is_column_winning := true
 		for j, ele := range row {
-			is_row_winning = is_row_winning &&  ele.mark
+			is_row_winning = is_row_winning && ele.mark
 			is_column_winning = is_column_winning && bingo[j][i].mark
 		}
 		if is_row_winning || is_column_winning {
@@ -75,21 +86,20 @@ func value_of_bingo(bingo Bingo) int {
 	return result
 }
 
-func draw_number(bingos []Bingo, number int) (int, bool) {
+func draw_number(bingos []Bingo, number int) (int, int, bool) {
 	for k, bingo := range bingos {
 		for i, row := range bingo {
-			for j, ele := range  row {
+			for j, ele := range row {
 				if number == ele.value {
 					bingos[k][i][j].mark = true
 				}
 			}
 		}
-		if is_winning(bingo) {
-			fmt.Println(bingo)
-			return number * value_of_bingo(bingo), true
+		if is_winning(bingos[k]) {
+			return number * value_of_bingo(bingos[k]), k, true
 		}
 	}
-	return 0, false
+	return 0, -1, false
 }
 
 func part1() {
@@ -97,7 +107,7 @@ func part1() {
 	start := time.Now()
 	numbers, bingos := read_file("INPUT")
 	for _, number := range numbers {
-		val, win := draw_number(bingos, number)
+		val, _, win := draw_number(bingos, number)
 		if win {
 			result = val
 			break
@@ -110,6 +120,15 @@ func part1() {
 func part2() {
 	var result int
 	start := time.Now()
+	numbers, bingos := read_file("INPUT")
+	for _, number := range numbers {
+		val, index, win := draw_number(bingos, number)
+		for win {
+			bingos = append(bingos[:index], bingos[index+1:]...)
+			result = val
+			val, index, win = draw_number(bingos, number)
+		}
+	}
 	end := time.Now()
 	fmt.Println("Part 2 [", end.Sub(start), "]:", result)
 }
