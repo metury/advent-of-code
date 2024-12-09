c8b289067295513558a45b744ff7bcfebcff222f
Tomáš Turek <tom.turek@atlas.cz>
2024-12-09 13:21:59 +0100

    updated solution to day 9

--- 2024/09/info.md
+++ 2024/09/info.md
@@ -1,4 +1,7 @@
 #### Part 1
 
+Today I used two different approaches for each part. In the first part I recreated the disc and by following two indices (going forward and backward) I swap spaces and files between each other. Lastly I compute the sum.
+
 #### Part 2
 
+For the second part I created a structure that holds the content of the place, its size and possibly index of a file. Then I also go backwards and forward at the same time and if I find a file I will try to find a big enough space and then put it there. Afterwards I partition the space if there is some left and compact consecutive spaces to just one.
--- 2024/09/main.go
+++ 2024/09/main.go
@@ -10,14 +10,15 @@ import (
 )
 
 const (
-	SPACE = 0
-	FILE  = 1
+	SPACE   = 0
+	FILE    = 1
+	INVALID = -1
 )
 
-type Format struct {
-	format int
-	size   int
-	index  int
+type Partition struct {
+	content int
+	size    int
+	index   int
 }
 
 func read_file(file_path string) []int {
@@ -34,61 +35,63 @@ func read_file(file_path string) []int {
 	return format
 }
 
+func init_array(size, val int) []int {
+	array := make([]int, size)
+	for i := range array {
+		array[i] = val
+	}
+	return array
+}
+
 func recreate_disk(format []int) []int {
 	var disk []int
 	id := 0
 	for i := 0; i < len(format); i++ {
 		if i%2 == 0 {
-			partition := make([]int, format[i])
-			for i := range partition {
-				partition[i] = id
-			}
+			partition := init_array(format[i], id)
 			id++
 			disk = append(disk, partition[:]...)
 		} else {
-			partition := make([]int, format[i])
-			for i := range partition {
-				partition[i] = -1
-			}
+			partition := init_array(format[i], INVALID)
 			disk = append(disk, partition[:]...)
 		}
 	}
 	return disk
 }
 
-func create_my_format(format []int) []Format {
-	var prop_format []Format
+func create_my_format(format []int) []Partition {
+	var prop_format []Partition
 	index := 0
 	for i := 0; i < len(format); i++ {
 		if i%2 == 0 {
-			prop_format = append(prop_format, Format{FILE, format[i], index})
+			prop_format = append(prop_format, Partition{FILE, format[i], index})
 			index++
 		} else {
-			prop_format = append(prop_format, Format{SPACE, format[i], -1})
+			prop_format = append(prop_format, Partition{SPACE, format[i], INVALID})
 		}
 	}
 	return prop_format
 }
 
-func reallocate(disk *[]int) {
+func shrink(disk *[]int) {
 	a, i := 0, len(*disk)-1
 	for a < i {
-		if (*disk)[a] == -1 && (*disk)[i] != -1 {
+		if (*disk)[a] == INVALID && (*disk)[i] != INVALID {
 			(*disk)[a] = (*disk)[i]
-			(*disk)[i] = -1
+			(*disk)[i] = INVALID
 			a++
 			i--
-		} else if (*disk)[a] != -1 {
+		} else if (*disk)[a] != INVALID {
 			a++
-		} else if (*disk)[i] == -1 {
+		} else if (*disk)[i] == INVALID {
 			i--
 		}
 	}
 }
 
-func compact(format *[]Format) {
+func compact(format *[]Partition) {
 	for i := 1; i < len(*format); i++ {
-		if (*format)[i-1].format == SPACE && (*format)[i].format == SPACE {
+		if (*format)[i-1].content == SPACE && (*format)[i].content == SPACE {
 			(*format)[i-1].size += (*format)[i].size
 			*format = append((*format)[:i], (*format)[i+1:]...)
 			i--
@@ -96,36 +99,39 @@ func compact(format *[]Format) {
 	}
 }
 
-func reallocate2(format *[]Format) {
-	for i := len(*format) - 1; i >= 0; i-- {
-		if (*format)[i].format == FILE {
-			for a := 0; a < i; a++ {
-				rest := (*format)[a].size - (*format)[i].size
-				found := false
-				if (*format)[a].format == SPACE && rest >= 0 {
-					(*format)[a].format = FILE
-					(*format)[a].size = (*format)[i].size
-					(*format)[a].index = (*format)[i].index
-					(*format)[i].format = SPACE
-					found = true
-				}
-				if found && rest > 0 {
-					new_format := make([]Format, len(*format)+1)
-					for j := 0; j < a+1; j++ {
-						new_format[j] = (*format)[j]
-					}
-					new_format[a+1] = Format{SPACE, rest, -1}
-					for j := a + 2; j < len(new_format); j++ {
-						new_format[j] = (*format)[j-1]
-					}
-					*format = new_format
-				}
-				if found {
-					break
-				}
+func insert(format *[]Partition, index int, part Partition) {
+	new_format := make([]Partition, len(*format)+1)
+	for j := 0; j < index+1; j++ {
+		new_format[j] = (*format)[j]
+	}
+	new_format[index+1] = part
+	for j := index + 2; j < len(new_format); j++ {
+		new_format[j] = (*format)[j-1]
+	}
+	*format = new_format
+}
 
+func reallocate_file(file_index int, format *[]Partition) {
+	for a := 0; a < file_index; a++ {
+		rest := (*format)[a].size - (*format)[file_index].size
+		if (*format)[a].content == SPACE && rest >= 0 {
+			(*format)[a].content = FILE
+			(*format)[a].size = (*format)[file_index].size
+			(*format)[a].index = (*format)[file_index].index
+			(*format)[file_index].content = SPACE
+			if rest > 0 {
+				insert(format, a, Partition{SPACE, rest, -1})
+				compact(format)
 			}
-			compact(format)
+			return
+		}
+	}
+}
+
+func reallocate(format *[]Partition) {
+	for i := len(*format) - 1; i >= 0; i-- {
+		if (*format)[i].content == FILE {
+			reallocate_file(i, format)
 		}
 	}
 }
@@ -133,11 +139,10 @@ func reallocate2(format *[]Format) {
 func part1() {
 	var result int
 	start := time.Now()
-	format := read_file("INPUT")
-	disk := recreate_disk(format)
-	reallocate(&disk)
+	disk := recreate_disk(read_file("INPUT"))
+	shrink(&disk)
 	for i, el := range disk {
-		if el != -1 {
+		if el != INVALID {
 			result += i * el
 		}
 	}
@@ -148,14 +153,13 @@ func part1() {
 func part2() {
 	var result int
 	start := time.Now()
-	format := read_file("INPUT")
-	my_format := create_my_format(format)
-	reallocate2(&my_format)
+	format := create_my_format(read_file("INPUT"))
+	reallocate(&format)
 	position := 0
-	for i := 0; i < len(my_format); i++ {
-		for j := 0; j < my_format[i].size; j++ {
-			if my_format[i].format == FILE {
-				result += my_format[i].index * position
+	for i := 0; i < len(format); i++ {
+		for j := 0; j < format[i].size; j++ {
+			if format[i].content == FILE {
+				result += format[i].index * position
 			}
 			position++
 		}
