01793e01f1a00e4f1dd9d5748d0c4d4a58e1c9a6
Tomáš Turek <tom.turek@atlas.cz>
2024-12-24 20:34:03 +0100

    updated solution to day 24

--- 2024/24/info.md
+++ 2024/24/info.md
@@ -4,3 +4,4 @@ For the first part we only evaluate all of the gates and recreate the number.
 
 #### Part 2
 
+For the second part it is somewhat easier to look up by hand. There are three easy candidates with `z` values. next you may recreate a mermaid diagram by using regex `([0-9a-z]+) ((?:OR)|(?:AND)|(?:XOR)) ([0-9a-z]+) -> ([0-9a-z]+)` and replace it by `\1 -- "\2" --> \4;\n\3 -- "\2" --> \4;`.
--- 2024/24/main.go
+++ 2024/24/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"regexp"
 	"sort"
+	"strings"
 	"time"
 )
 
@@ -16,21 +17,22 @@ const (
 	Reset  = "[0m"
 )
 
-func print_result(dur time.Duration, part, result int) {
-	fmt.Println("Part " + fmt.Sprint(part) + " [" + Blue + fmt.Sprint(dur) + Reset + "]: " + Yellow + fmt.Sprint(result) + Reset)
+func print_result(dur time.Duration, part int, result string) {
+	fmt.Println("Part " + fmt.Sprint(part) + " [" + Blue + fmt.Sprint(dur) + Reset + "]: " + Yellow + result + Reset)
 }
 
 const (
 	And = 0
-	Or = 1
+	Or  = 1
 	Xor = 2
 )
 
 type Gate struct {
-	inputs [2]string
-	output string
-	gate int
+	inputs    [2]string
+	output    string
+	gate      int
 	evaluated bool
+	switched  bool
 }
 
 func read_file(file_path string) ([]Gate, map[string]bool) {
@@ -51,12 +53,12 @@ func read_file(file_path string) ([]Gate, map[string]bool) {
 	}
 	for _, found := range regex_gates.FindAllStringSubmatch(string(content), -1) {
 		switch found[2] {
-			case "OR":
-				gates = append(gates,Gate {[2]string{found[1], found[3]}, found[4], Or, false})
-			case "XOR":
-				gates = append(gates,Gate {[2]string{found[1], found[3]}, found[4], Xor, false})
-			case "AND":
-				gates = append(gates,Gate {[2]string{found[1], found[3]}, found[4], And, false})
+		case "OR":
+			gates = append(gates, Gate{[2]string{found[1], found[3]}, found[4], Or, false, false})
+		case "XOR":
+			gates = append(gates, Gate{[2]string{found[1], found[3]}, found[4], Xor, false, false})
+		case "AND":
+			gates = append(gates, Gate{[2]string{found[1], found[3]}, found[4], And, false, false})
 		}
 	}
 	return gates, values
@@ -81,7 +83,7 @@ func eval_bit(values *map[string]bool, first_symbol byte) (int, []int) {
 		} else {
 			bits = append(bits, 0)
 		}
-		acc = 2*acc
+		acc = 2 * acc
 	}
 	return sum, bits
 }
@@ -103,12 +105,12 @@ func evaluate_gates(gates *[]Gate, values *map[string]bool) {
 			if first_ok && second_ok {
 				(*gates)[i].evaluated = true
 				switch gate.gate {
-					case And:
-						(*values)[(*gates)[i].output] = first && second
-					case Or:
-						(*values)[(*gates)[i].output] = first || second
-					case Xor:
-						(*values)[(*gates)[i].output] = first != second
+				case And:
+					(*values)[(*gates)[i].output] = first && second
+				case Or:
+					(*values)[(*gates)[i].output] = first || second
+				case Xor:
+					(*values)[(*gates)[i].output] = first != second
 				}
 			}
 		}
@@ -118,32 +120,12 @@ func evaluate_gates(gates *[]Gate, values *map[string]bool) {
 func find_candidates(gates []Gate) []string {
 	var candidates []string
 	for _, gate := range gates {
-		if gate.output[0] == 'z' && gate.gate != Xor{
+		if gate.output[0] == 'z' && gate.gate != Xor {
 			candidates = append(candidates, gate.output)
 		}
 	}
 	sort.Strings(candidates)
 	return candidates[:len(candidates)-1]
-}
-
-func find_xors(gates []Gate) []string {
-	var candidates []string
-	for _, gate := range gates {
-		if gate.output[0] != 'z' && gate.gate == Xor{
-			candidates = append(candidates, gate.output)
-		}
-	}
-	return candidates
-}
-
-func create_tuples(candidates []string, xors []string) [][2]string {
-	var tuples [][2]string
-	for _, candidate := range candidates {
-		for _, xor := range xors {
-			tuples = append(tuples, [2]string{candidate, xor})
-		}
-	}
-	return tuples
 }
 
 func part_one() {
@@ -153,15 +135,16 @@ func part_one() {
 	evaluate_gates(&gates, &values)
 	result, _ = eval_bit(&values, 'z')
 	end := time.Now()
-	print_result(end.Sub(start), 1, result)
+	print_result(end.Sub(start), 1, fmt.Sprint(result))
 }
 
 func part_two() {
-	var result int
+	var result string
 	start := time.Now()
 	gates, _ := read_file("INPUT")
-	candidates := create_tuples(find_candidates(gates), find_xors(gates))
-	fmt.Println(candidates, len(find_xors(gates)))
+	candidates := find_candidates(gates)
+	sort.Strings(candidates)
+	result = strings.Join(candidates, ",")
 	end := time.Now()
 	print_result(end.Sub(start), 2, result)
 }
