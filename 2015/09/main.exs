40204f06d6d851b09fbd7104f09b48fd813db7d4
Tomáš Turek <tom.turek@atlas.cz>
2025-01-01 11:47:13 +0100

    shrinked to one code

--- 2015/09/main.exs
+++ 2015/09/main.exs
@@ -24,24 +24,10 @@ if {:ok, content} = File.read("INPUT") do
 	|> Enum.map(fn [town1, town2, {dist, _}] -> [{town1, town2, dist}, {town2, town1, dist}] end)
 	|> List.flatten()
 	|> Enum.sort(fn {t1, t2, _}, {t3, t4, _} -> t1 < t3 or (t1 == t3 and t2 < t4) end)
-	result = Enum.map(distances, fn {town	, _, _} -> town end)
+	lengths = Enum.map(distances, fn {town	, _, _} -> town end)
 	|> Enum.dedup()
 	|> Perms.perms()
 	|> Enum.map(fn towns -> Cycle.compute(distances, towns) end)
-	|> Enum.min()
-	IO.puts("Part 1: #{result}")
-end
-
-if {:ok, content} = File.read("INPUT") do
-	distances = Regex.scan(~r/([a-zA-Z]+) to ([a-zA-Z]+) = ([0-9]+)/, String.trim(content))
-	|> Enum.map(fn [_, town1, town2, dist] -> [town1, town2, Integer.parse(dist)] end)
-	|> Enum.map(fn [town1, town2, {dist, _}] -> [{town1, town2, dist}, {town2, town1, dist}] end)
-	|> List.flatten()
-	|> Enum.sort(fn {t1, t2, _}, {t3, t4, _} -> t1 < t3 or (t1 == t3 and t2 < t4) end)
-	result = Enum.map(distances, fn {town	, _, _} -> town end)
-	|> Enum.dedup()
-	|> Perms.perms()
-	|> Enum.map(fn towns -> Cycle.compute(distances, towns) end)
-	|> Enum.max()
-	IO.puts("Part 2: #{result}")
+	IO.puts("Part 1: #{Enum.min(lengths)}")
+	IO.puts("Part 2: #{Enum.max(lengths)}")
 end
