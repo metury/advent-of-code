848dacdee74e5b0917d6f3c379b3878c25c2c6d1
Tomáš Turek <tom.turek@atlas.cz>
2025-01-01 11:26:37 +0100

    solved day 9

---
+++ 2015/09/info.md
@@ -0,0 +1,7 @@
+#### Part 1
+
+We will create all permutations of towns and then compute their distance, after that we find the minimal one.
+
+#### Part 2
+
+In the second part find the maximal one.
--- 2015/09/main.exs
+++ 2015/09/main.exs
@@ -1,12 +1,22 @@
 IO.puts("Year 2015 day 9 - All in a Single Night")
 
-defmodule Cycle do
-	def recfind([dist | distances], length, towns) do
+defmodule Perms do
+  def perms([]), do: [[]]
 
+  def perms(l) do
+    for h <- l, t <- perms(l -- [h]),
+      do: [h|t]
+  end
+end
+
+defmodule Cycle do
+	def compute(_, [_]) do
+		0
 	end
 
-	def find(distances, towns) do
-		recfind(distances, 0, towns)
+	def compute(distances, [t1, t2 | towns]) do
+		{_, _, dist} = Enum.find(distances, fn {t3, t4, _} -> t3 == t1 and t4 == t2 end)
+		dist + compute(distances, [t2 | towns])
 	end
 end
 
@@ -16,10 +26,24 @@ if {:ok, content} = File.read("INPUT") do
 	|> Enum.map(fn [town1, town2, {dist, _}] -> [{town1, town2, dist}, {town2, town1, dist}] end)
 	|> List.flatten()
 	|> Enum.sort(fn {t1, t2, _}, {t3, t4, _} -> t1 < t3 or (t1 == t3 and t2 < t4) end)
-	towns = distances |> Enum.map(fn {town	, _, _} -> town end) |> Enum.dedup()
-	IO.puts("Part 1: #{0}")
+	result = Enum.map(distances, fn {town	, _, _} -> town end)
+	|> Enum.dedup()
+	|> Perms.perms()
+	|> Enum.map(fn towns -> Cycle.compute(distances, towns) end)
+	|> Enum.min()
+	IO.puts("Part 1: #{result}")
 end
 
 if {:ok, string} = File.read("INPUT") do
-	IO.puts("Part 2: #{0}")
+distances = Regex.scan(~r/([a-zA-Z]+) to ([a-zA-Z]+) = ([0-9]+)/, String.trim(content))
+	|> Enum.map(fn [_, town1, town2, dist] -> [town1, town2, Integer.parse(dist)] end)
+	|> Enum.map(fn [town1, town2, {dist, _}] -> [{town1, town2, dist}, {town2, town1, dist}] end)
+	|> List.flatten()
+	|> Enum.sort(fn {t1, t2, _}, {t3, t4, _} -> t1 < t3 or (t1 == t3 and t2 < t4) end)
+	result = Enum.map(distances, fn {town	, _, _} -> town end)
+	|> Enum.dedup()
+	|> Perms.perms()
+	|> Enum.map(fn towns -> Cycle.compute(distances, towns) end)
+	|> Enum.max()
+	IO.puts("Part 2: #{result}")
 end
