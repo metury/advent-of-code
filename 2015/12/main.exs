52c9aa654fb1354324af5027a3d3dddd20d6160c
Tomáš Turek <tom.turek@atlas.cz>
2025-01-01 14:12:47 +0100

    small update

--- 2015/12/main.exs
+++ 2015/12/main.exs
@@ -5,10 +5,10 @@ if {:ok, content} = File.read("INPUT") do
 	|> Enum.map(fn [x] -> if {val, _ } = Integer.parse(x) do val end end)
 	|> Enum.sum
 	IO.puts("Part 1: #{result}")
-	{result, _}= Regex.scan(~r/(?:\-{0,1}[0-9]+)|(?:red)|(?:\})/, content)
+	{result, _}= Regex.scan(~r/(?:\-{0,1}[0-9]+)|(?:\:\s*"red")|(?:\})/, content)
 	|> List.foldl({0, 0}, fn [x], {sum, red} ->
 	cond do
-		x == "red" ->
+		x == ":\"red\"" ->
 			{sum, red + 1}
 		x == "}" ->
 			{sum, Enum.max([0, red-1])}
